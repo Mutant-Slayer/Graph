39689d91311c445c69b43dcf9fb466c3619049cb
anas shikoh <anas.shikoh@kukufm.com>
2025-10-25 07:02:10 +0530

    adjacency matrix

---
+++ src/AdjacencyMatrix.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val edgeList = listOf(
+        listOf(1, 2),
+        listOf(2, 3),
+        listOf(3, 4),
+        listOf(4, 2),
+        listOf(1, 3)
+    )
+    val n = 5
+
+    val adjacencyMatrix = List(n) { MutableList(n) { 0 } }
+
+    for (i in 0 until edgeList.size) {
+        val a = edgeList[i][0]
+        val b = edgeList[i][1]
+        adjacencyMatrix[a][b] = 1
+    }
+
+    printAdjacencyMatrix(n - 1, adjacencyMatrix)
+}
+
+private fun printAdjacencyMatrix(n: Int, adjacencyMatrix: List<MutableList<Int>>) {
+
+    for (i in 1..n) {
+        print("Node $i => Neighbours : ")
+        for (j in 1..n) {
+            if (adjacencyMatrix[i][j] == 1) {
+                print("$j ")
+            }
+        }
+        println()
+    }
+}
